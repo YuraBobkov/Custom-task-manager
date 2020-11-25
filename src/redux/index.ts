35d54f53daddffb712925708103567f7734de58e
Yuriy Bobkov <yuriy.bobkov@upsilonit.com>
2020-11-25 22:35:07 +0300

    add saga middleware

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,12 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { useSelector, TypedUseSelectorHook } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [...getDefaultMiddleware(), sagaMiddleware];
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware,
   devTools: true,
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof rootReducer>;
 
